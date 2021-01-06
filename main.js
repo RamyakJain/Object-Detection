b8833fa7bcfc7c6efd6575ef8555a4f4ac280399
RamyakJain <73877400+RamyakJain@users.noreply.github.com>
2021-01-06 20:34:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+img = ""
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+function setup(){
+    canvas= createCanvas(640, 420);
+    canvas.center();
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill('#cf1515');
+    text('Dog', 35, 70);
+    noFill();
+    stroke('#cf1515');
+    rect(30, 60, 350, 350);
+}
