313aae3a834bb15d22abff61c222c61f3784d89b
RamyakJain <73877400+RamyakJain@users.noreply.github.com>
2021-01-06 20:44:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function setup(){
 function draw(){
     image(img, 0, 0, 640, 420);
     fill('#cf1515');
-    text('Dog', 35, 70);
+    textSize(18);
+    text('Dog', 35, 79);
     noFill();
     stroke('#cf1515');
     rect(30, 60, 350, 350);
